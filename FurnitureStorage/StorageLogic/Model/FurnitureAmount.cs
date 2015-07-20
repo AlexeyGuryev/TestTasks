81fe1f77eb7f442bf616a7f9629fae3ed9495521
alexey gurev <alguryew@gmail.com>
2015-07-20 18:54:15 +0700

    Старт переработки моделей под возможности EF

---
+++ FurnitureStorage/StorageLogic/Model/FurnitureAmount.cs
@@ -0,0 +1,17 @@
+﻿namespace StorageLogic.Model
+{
+    public class FurnitureAmount
+    {
+        public string FurnitureType { get; set; }
+        public int Count { get; set; }
+    }
+
+    //public enum FurnitureType
+    //{
+    //    Chair,
+    //    Desk,
+    //    Table,
+    //    Bed,
+    //    Sofa
+    //}
+}
---
+++ FurnitureStorage/StorageLogic/Service/RoomFurnitureService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StorageLogic.Exception;
+using StorageLogic.Model;
+
+namespace StorageLogic.Service
+{
+    public class RoomFurnitureService
+    {
+        public static void MoveFurnitureBetweenRooms(Room roomFrom, Room roomTo, DateTime removeDate, string furnitureType)
+        {
+            //var furnitures = roomToRemove.Furnitures.Keys.ToList();
+            //foreach (var furniture in furnitures)
+            //{
+            //    var countValue = roomToRemove.Furnitures[furniture];
+            //    roomTo.AddFurniture(furniture, countValue);
+            //    roomFrom.RemoveFurniture(furniture, countValue);
+            //}
+            //roomFrom.RemoveDate = removeDate;
+
+            //AddRoomStateIfChanged(roomToRemove, removeDate);
+            //AddRoomStateIfChanged(transferRoom, removeDate);
+        }
+
+        public static void AddFurnitureToRoom(string furnitureType, Room roomName, DateTime createFurnitureDate)
+        {
+            //var room = GetRoom(roomName, createFurnitureDate);
+            //CheckIfRoomStateIsLatest(room, createFurnitureDate);
+
+            //room.AddFurniture(furnitureType);
+            //AddRoomStateIfChanged(room, createFurnitureDate);
+        }
+
+        //private void CheckIfRoomStateIsLatest(Room room, DateTime date)
+        //{
+        //    var latestState = _repository.GetLatestRoomState(room.Name, DateTime.MaxValue);
+        //    if (latestState.StateDate > date)
+        //    {
+        //        throw new DateConsistenceException("There is later changes for room {0}", room.Name);
+        //    }
+        //}
+
+        public void MoveFurniture(string furnitureType, string roomNameFrom, string roomNameTo,
+            DateTime moveFurnitureDate)
+        {
+            //var roomFrom = GetRoom(roomNameFrom, moveFurnitureDate);
+            //var roomTo = GetRoom(roomNameTo, moveFurnitureDate);
+
+            //CheckIfRoomStateIsLatest(roomFrom, moveFurnitureDate);
+            //CheckIfRoomStateIsLatest(roomTo, moveFurnitureDate);
+
+            //if (roomFrom.Furnitures.ContainsKey(furnitureType)
+            //    && roomFrom.Furnitures[furnitureType] > 0)
+            //{
+            //    roomTo.AddFurniture(furnitureType);
+            //    roomFrom.RemoveFurniture(furnitureType);
+
+            //    AddRoomStateIfChanged(roomFrom, moveFurnitureDate);
+            //    AddRoomStateIfChanged(roomTo, moveFurnitureDate);
+            //}
+            //else
+            //{
+            //    throw new ItemNotFoundException("The furniture of type {0} is not exists in room {1}",
+            //        furnitureType, roomFrom);
+            //}
+        }
+    }
+}
